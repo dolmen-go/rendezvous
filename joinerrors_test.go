427aa3e4e9845252c3255ac0733e01ab4f7c13fd
Olivier Mengué <dolmen@cpan.org>
2023-12-20 22:41:56 +0100

    More joinErrors coverage
    Add full coverage for the pre-Go 1.20 local implementation of
    joinErrors.

--- joinerrors_test.go
+++ joinerrors_test.go
@@ -3,6 +3,7 @@ package rendezvous
 import (
 	"context"
 	"errors"
+	"regexp"
 	"testing"
 )
 
@@ -19,5 +20,28 @@ func TestJoinErrors(t *testing.T) {
 	var me myError
 	if !errors.As(joinErrors(context.Canceled, myError{}), &me) {
 		t.Error("errors.As failure")
+	}
+
+	err := joinErrors(myError{}, myError{})
+	if err == nil {
+		t.Fatal("nil unexpected")
+	}
+	var errs []error
+	if err := err.(interface{ Unwrap() []error }); err == nil {
+		t.Fatal("Unwrap() []error expected")
+	} else {
+		errs = err.Unwrap()
+	}
+	if len(errs) != 2 {
+		t.Fatal("len 2 expected")
+	}
+
+	_ = errs[0].(myError)
+	_ = errs[1].(myError)
+
+	str := err.Error()
+	re := regexp.MustCompile(regexp.QuoteMeta(myError{}.Error()))
+	if len(re.FindAllString(str, -1)) != 2 {
+		t.Fatal("2 occurences of (myError{}).Error() expected")
 	}
 }
