84dca5e62535e5b9a7a7a1c17b610d174b82a518
Olivier Mengué <dolmen@cpan.org>
2023-12-20 23:08:53 +0100

    TestMain: guard display of godebug metrics behind flag
    Add "go test" flag "-godebug-metrics" to enable the dump of godebug
    metrics.

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package rendezvous_test
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"runtime/metrics"
@@ -9,23 +10,28 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	// call flag.Parse() here if TestMain uses flags
+	var showMetrics bool
+	flag.BoolVar(&showMetrics, "godebug-metrics", false, "show godebug metrics")
+	flag.Parse()
+
 	exitCode := m.Run()
 
-	// Dump godebug metrics to investigate https://github.com/golang/go/issues/64649
-	descs := metrics.All()
-	var godebugMetrics []metrics.Sample
-	for i := range descs {
-		// skip metrics.KindBad
-		if descs[i].Kind == metrics.KindUint64 && strings.HasPrefix(descs[i].Name, "/godebug/") && strings.HasSuffix(descs[i].Name, ":events") {
-			godebugMetrics = append(godebugMetrics, metrics.Sample{Name: descs[i].Name})
+	if showMetrics {
+		// Dump godebug metrics to investigate https://github.com/golang/go/issues/64649
+		descs := metrics.All()
+		var godebugMetrics []metrics.Sample
+		for i := range descs {
+			// skip metrics.KindBad
+			if descs[i].Kind == metrics.KindUint64 && strings.HasPrefix(descs[i].Name, "/godebug/") && strings.HasSuffix(descs[i].Name, ":events") {
+				godebugMetrics = append(godebugMetrics, metrics.Sample{Name: descs[i].Name})
+			}
 		}
-	}
-	if len(godebugMetrics) > 0 {
-		metrics.Read(godebugMetrics)
-		for i := range godebugMetrics {
-			if godebugMetrics[i].Value.Uint64() > 0 {
-				fmt.Printf("%s: %v\n", godebugMetrics[i].Name, godebugMetrics[i].Value.Uint64())
+		if len(godebugMetrics) > 0 {
+			metrics.Read(godebugMetrics)
+			for i := range godebugMetrics {
+				if godebugMetrics[i].Value.Uint64() > 0 {
+					fmt.Printf("%s: %v\n", godebugMetrics[i].Name, godebugMetrics[i].Value.Uint64())
+				}
 			}
 		}
 	}
